1373607efbbb36cb14d04e8660b21d68b89c14b2
Nathan <nathaniwilliamsuk@gmail.com>
2020-03-20 10:55:59 +0000

    refactor to do better message parsing

--- jamesBot/jamesBot.py
+++ jamesBot/jamesBot.py
@@ -10,6 +10,14 @@ from googletrans import Translator
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+
+class Response:
+    def __init__(self, response_message: str, response_parameters: [],action=None):
+        self.responseMessage = response_message
+        self.responseParameters = response_parameters
+        self.action=action
+
+
 robCommands = ['rob steal', 'rob quote context', 'rob catch']
 thingsToDo = ['hopping over to minecraft.ssago.org',
               'joining in on or run a virtual event',
@@ -20,15 +28,40 @@ thingsToDo = ['hopping over to minecraft.ssago.org',
               'hop into a voice channel with your friends',
               'go outside for some fresh air',
               'have a nice bite to eat',
-              'cook a chocolate brownie',
-              'cook a homemade pizza',
-              'Have a nice drink of water',
+              'cooking a chocolate brownie',
+              'cooking a homemade pizza',
+              'have a nice drink of water',
               'checkout some photos on the photo channel',
               'checkout some memes on the memes channel',
               'discuss some tv on the tv channel',
               'develop a mascotBot to talk to be my friend',
               ]
+def how_am_i(j, message:Message):
+    if j.translationEnabled:
+        result = 'I will translate every 1/' + str(j.translationFrequency) + ' messages.\n'
+    else:
+        result =  'I was told to shutup :( baaa please re-enable me... baaaa\n'
+    if len(j.stealAttempts) > 0:
+        result = result + 'And these people have recently tried to steal me: ' + listPrint(j.stealAttempts)
+    return result
 
+def friend(j,message:Message):
+    if message.author.mention in j.friends:
+        return 'I am already your friend! baaaa'
+    else:
+        j.friends.add(message.author.mention)
+        return 'sure you can! baaaaa'
+
+messages = {
+    'i am good': Response('{0} Well that\'s nice to see, maybe you could try {1} to make your day even better.',['mention', 'thingstodo']),
+    'i am not good': Response('{0} oh no, might I suggest that you {1} to cheer you up?', ['mention', 'thingstodo']),
+    'i am excellent': Response('{0} Awesome, maybe you can spread your joy by {1}.', ['mention', 'thingstodo']),
+    'what can i do around here?': Response('{0} You could {1}.', ['mention', 'thingstodo']),
+    'who are your friends': Response('I am friends with: {0}', ['friends']),
+    'who are you?': Response('I am one of MSAGM Aber SSAGO\'s mascots, I normally reside with the Secretary.\nI am friends with {0}',['friends']),
+    'how are you': Response('Alive and well :) Baaaa.\n{0} and how are you?', ['actionresult'], action=how_am_i),
+    'can i be your friend': Response('{0}',['actionresult'],action=friend)
+}
 
 def listPrint(l: []):
     m = ''
@@ -91,49 +124,28 @@ class JamesTheSheep(discord.Client):
 
     async def converse(self, message: Message, m: str):
         m = m.lower()
-        if m.startswith('how are you'):
-            result = 'Alive and well :) Baaaa\n'
-            if self.translationEnabled:
-                result = result + 'I will translate every 1/' + str(self.translationFrequency) + ' messages.\n'
-            else:
-                result = result + 'I was told to shutup :( baaa please re-enable me... baaaa\n'
-            if len(self.stealAttempts) > 0:
-                result = result + 'And these people have recently tried to steal me: ' + listPrint(
-                    self.stealAttempts) + '\n'
-            result = result + 'and how are you?'
-            await message.channel.send(result)
-            return True
-        elif m.startswith('who are you?'):
-            result = 'I am one of MSAGM Aber SSAGO\'s mascots, I normally reside with the Secretary.\n'
-            result = result + 'I am friends with' + listPrint(self.friends)
-            await message.channel.send(result)
-            return True
-        elif m.startswith('can i be your friend'):
-            if message.author.mention in self.friends:
-                await message.channel.send('I am already your friend! baaaa')
-            else:
-                self.friends.add(message.author.mention)
-                await message.channel.send('sure you can! baaaaa')
-            return True
-        elif m.startswith('who are your friends'):
-            await message.channel.send('I am friends with: ' + listPrint(self.friends))
-            return True
-        elif m.startswith('i am good'):
-            await message.channel.send(
-                'Well that\'s nice to see, maybe you could try {0} to make your day even better.'.format(
-                    thingsToDo[randint(0, len(thingsToDo) - 1)]))
-            return True
-        elif m.startswith('i am not good'):
-            await message.channel.send(
-                'oh no, might I suggest that you {0} to cheer you up?'.format(
-                    thingsToDo[randint(0, len(thingsToDo) - 1)]))
-            return True
-        elif m.startswith('i am excellent'):
-            await message.channel.send(
-                'Awesome, maybe you can spread your joy by {0}.'.format(
-                    thingsToDo[randint(0, len(thingsToDo) - 1)]))
-            return True
+        for n in messages.keys():
+            if m.startswith(n):
+                response:Response = messages.get(n)
+                response_parameters = response.responseParameters
+                action_result = ''
+                if response.action is not None:
+                    action_result = response.action(self,message)
+                for i,j in enumerate(response.responseParameters):
+                    if j == 'mention':
+                        response_parameters[i] = message.author.mention
+                    elif j == 'friends':
+                        response_parameters[i] = listPrint(self.friends)
+                    elif j == 'thingstodo':
+                        response_parameters[i] = thingsToDo[randint(0,len(thingsToDo)-1)]
+                    elif j == 'actionresult':
+                        response_parameters[i] = action_result
+
+                await message.channel.send(response.responseMessage.format(*response_parameters))
+                return True
         return False
+
+
 
     async def runCommand(self, message: Message, m: str):
         """Dispatch method"""
@@ -194,11 +206,14 @@ class JamesTheSheep(discord.Client):
                                    'Who are you?\n'
                                    'Can I be your friend?\n'
                                    'Who are your friends?\n'
+                                   'What can I do around here?\n'
                                    '```')
         return True
 
     async def returnFalse(self, message, command):
         return False
+
+
 
 
 client = JamesTheSheep()
